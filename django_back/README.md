b051f19bf272e2aeac9aa97000d7841337c17520
PSI <tkddls8848@gmail.com>
2020-12-25 12:59:49 +0900

    make react app with Django

--- django_back/README.md
+++
@@ -1,8 +0,0 @@
-파이썬 + 리액트 홈페이지 프로젝트
-
-해당 영상 참고
-https://www.youtube.com/watch?v=Uyei2iDA4Hs&list=PLillGF-RfqbbRA-CIUxlxkUpbq0IFkX60&index=1
-
-python3
-Django
-Django-rest-framework
---
+++ django_back/frontend/src/components/App.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+
+import Header from './layout/header';
+
+class App extends Component {
+    render(){
+        return (
+            <Header />
+        )
+    }
+}
+
+ReactDOM.render(<App />, document.getElementById('app'));
