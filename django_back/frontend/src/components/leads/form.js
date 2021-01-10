43b04a2d3fbfe1796609e0ca35d9d85f71e24424
PSI <tkddls8848@gmail.com>
2021-01-10 12:46:33 +0900

    build REST API app(add, delete, get)

--- django_back/frontend/src/components/leads/form.js
+++ django_back/frontend/src/components/leads/form.js
@@ -1,13 +1,60 @@
 import React, {Component} from 'react'
+import {connect} from 'react-redux'
+import PropTypes from 'prop-types'
+import {addLead} from '../../actions/leads'
 
-export class Forms extends Component  {
+export class Form extends Component {
+    state = {
+        name : "",
+        email : "",
+        message : "",
+    }
+
+    static PropTypes = {
+        addLead: PropTypes.func.isRequired
+    }
+
+    onChange = (e) => this.setState({
+        [e.target.name]: e.target.value
+    })
+
+    onSubmit = (e) => {
+        e.preventDefault()
+        const {name, email, message} = this.state
+        const lead = {name, email,message}
+        this.props.addLead(lead)
+    }
+
     render(){
+        const {name, email, message} = this.state
         return(
-            <div>
-                <h1>Forms</h1>
+            <div className="card card-body mt-4 mb-4">                
+                <h2>Add Lead</h2>
+                <form onSubmit={this.onSubmit}>
+                    <div className="form-group">
+                        <label>Name</label>
+                        <input className="form-control" type="text" name="name" onChange={this.onChange} value={name}>
+                        </input>
+                    </div>
+                    <div className="form-group">
+                        <label>Email</label>
+                        <input className="form-control" type="email" name="email" onChange={this.onChange} value={email}>
+                        </input>
+                    </div>
+                    <div className="form-group">
+                        <label>Message</label>
+                        <textarea className="form-control" type="text" name="message" onChange={this.onChange} value={message}>
+                        </textarea>
+                    </div>
+                    <div className="form-group">
+                        <button type="submit" className="btn btn-primary">Submit
+                        </button>
+                    </div>
+                </form>
             </div>
         )
     }
 }
 
-export default Forms
+export default connect(null,{addLead})(Form)
+
